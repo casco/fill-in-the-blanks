366c3aa8dcdbd49132f7eaee7bfc9aa22795e036
Alejandro Fernandez <alejandro.casco.fernandez@gmail.com>
2023-03-14 20:48:49 -0300

    almostthere

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [template, setTemplate] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/template')
+      .then(response => {
+        setTemplate(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []); // empty dependency array
+
+  // Replace variables in the template sentence with dropdown menus
+  const replaceVariables = (sentence) => {
+    const pattern = /\$(\w+)/g;
+    let newSentence = sentence;
+    let match;
+
+    while ((match = pattern.exec(sentence)) !== null) {
+      const [variable, name] = match;
+      const options = template.choices[name];
+      const dropdown = (
+        <select key={variable}>
+          {options.map(option => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      );
+      newSentence = newSentence.replace(variable, dropdown);
+    }
+    console.log(sentence);
+    return newSentence;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Fill in the blanks</h1>
+      {template && (
+        <p>{replaceVariables(template.sentence)}</p>
+      )}
+      {/* Add your submit button here */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+//root.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>
+//);
+
+
+setTimeout(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}, 10);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
